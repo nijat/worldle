aac7fa6bd314c899ad49661f3cfe6106f8c7231c
Nijat Rzayev <rzaev.nicat@gmail.com>
2022-02-15 11:09:42 +0100

    added city of day

--- src/hooks/useCountry.ts
+++ src/hooks/useCountry.ts
@@ -4,7 +4,7 @@ import { countriesWithImage, Country } from "../domain/countries";
 
 const forcedCountries: Record<string, string> = {
   "2022-02-02": "TD",
-  "2022-02-14": "MAS",
+  "2022-02-15": "MAS",
 };
 
 export function useCountry(dayString: string): [Country, number, number] {
